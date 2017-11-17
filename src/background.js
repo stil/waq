a244ac72a47007243fa4c4869cca4c56f988c3a7
Przemysław Ryciuk <p@ryciuk.com>
2017-11-17 23:55:56 +0100

    Improved WhatsApp tab detection on FF startup

--- src/background.js
+++ src/background.js
@@ -1,14 +1,44 @@
+function writeDebug(...args) {
+  const currentTime = new Date().toISOString();
+  console.debug(['WAQ', currentTime].concat(args));
+}
+
 let waTabId = null;
 let previousTabId;
+
+function spareWhatsAppTabCheck(foundCallback, notFoundCallback) {
+  // Sometimes addon is loaded before browser restores all tabs.
+  // When tabs are not fully restored, URL property is "about:blank".
+  // It means we need to use other method to check if WhatsApp tab has been restored.
+  // Here we compare favicon URLs, which is hacky but apparently works.
+
+  chrome.tabs.query(
+    { },
+    (result) => {
+      writeDebug('Spare check all tabs', result);
+      const faviconUrl = chrome.runtime.getURL('src/icons/icon.ico');
+      const waTab = result.find(t => t.favIconUrl === faviconUrl);
+      if (waTab) {
+        writeDebug('Spare check successful!', waTab);
+        foundCallback(waTab);
+      } else {
+        writeDebug('Spare check failed.');
+        notFoundCallback();
+      }
+    },
+  );
+}
 
 function findWhatsAppTab(foundCallback, notFoundCallback) {
   chrome.tabs.query(
     { url: 'https://web.whatsapp.com/*' },
     (result) => {
       if (result.length > 0) {
+        writeDebug('WhatsApp tab is running in background.');
         foundCallback(result[0]);
       } else {
-        notFoundCallback();
+        writeDebug('WhatsApp has not been found running in background.');
+        spareWhatsAppTabCheck(foundCallback, notFoundCallback);
       }
     },
   );
@@ -55,6 +85,7 @@ function onTabSwitchRequested() {
 
 // Race condition, tab restoring takes place at same time background script is executed.
 // https://bugzilla.mozilla.org/show_bug.cgi?id=1411603
+writeDebug('Addon has been loaded.');
 window.setTimeout(ensureWhatsAppRunning, 2000);
 
 chrome.browserAction.onClicked.addListener(onTabSwitchRequested);
@@ -67,7 +98,9 @@ chrome.commands.onCommand.addListener((command) => {
 
 chrome.tabs.onRemoved.addListener((tabId) => {
   if (tabId === waTabId) {
-    window.setTimeout(ensureWhatsAppRunning, 2000);
+    const reopenIntervalMs = 2000;
+    writeDebug(`Pinned tab has been closed. Reopening in ${reopenIntervalMs} ms.`);
+    window.setTimeout(ensureWhatsAppRunning, reopenIntervalMs);
   }
 });
 
